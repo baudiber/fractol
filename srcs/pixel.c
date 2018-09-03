63663f6111c67ac490b0ea1bbbb20a412a6fbfca
baudiber <audibertben@gmail.com>
2018-09-03 18:52:24 +0200

    fixed some colors in pixel.c

--- srcs/pixel.c
+++ srcs/pixel.c
@@ -6,7 +6,7 @@
 /*   By: baudiber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/06 19:55:11 by baudiber          #+#    #+#             */
-/*   Updated: 2018/08/06 19:55:24 by baudiber         ###   ########.fr       */
+/*   Updated: 2018/09/03 18:04:05 by baudiber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	set_rainbow(int i, t_setup *stp, int tid, t_xy *xy)
 	degrad = stp->tmp[tid].iteration_max / 7;
 	if (i < degrad)
  		stp->img[(int)xy->x + (int)xy->y * WIDTH] = ((i * 0xFF \
-			/ degrad + 30) << 16);
+			/ degrad /2 + 30) << 16);
 	else if (i < degrad * 2)
 		stp->img[(int)xy->x + (int)xy->y * WIDTH] = (0xFF << 16) + ((i * 0xFF / degrad * 2 + 30) << 8);
 	else if (i < degrad * 3)
