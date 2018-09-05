ef4a19d0df7fdd00c9025e134688b109b2bf4408
Benoît Audibert <baudibert@winter-mushroom.com>
2018-09-05 16:48:19 +0200

    added instructions for school work

---
+++ srcs/zoom.c
@@ -0,0 +1,18 @@
+save x and y 
+before zoom 
+
+after zoom add   mouse x / 5?   to saved x
+after zoom add   mouse y / 5?   to saved y
+x = old x
+y = old y
+while ++x < width + old x 
+while ++y < width + old y
+
+
+display x - old x 
+y - old y
+
+
+
+pan is same
+change old x old y
